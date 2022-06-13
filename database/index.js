9afb224808be386bd36d60fada1ec427142eb2c9
Cory Nickerson <essej4269@Hotmail.com>
2022-06-13 14:04:28 -0400

    test

--- database/index.js
+++ database/index.js
@@ -4,13 +4,13 @@ const { Pool } = require('pg');
 const pool = new Pool({
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 5432,
-  user: process.env.DB_USER || 'corynickerson',
-  password: process.env.DB_PASSWORD || '',
+  user: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || 'myPassword',
   database: process.env.DB_NAME || 'sdcreviews'
 });
 
 pool.on('error', (err, client) => {
-  console.error('Unexpected error on idle client', err)
+  console.log(err);
   process.exit(-1)
 })
 
